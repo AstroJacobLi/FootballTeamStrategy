0b25369a2840360d6a1182837627a85e21bf4a0c
Jiaxuan Li <jiaxuan_li@pku.edu.cn>
2020-02-15 00:11:34 +0800

    calculate motifs in a network

--- utils.py
+++ utils.py
@@ -5,6 +5,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
+def mcounter(gr, mo):
+    """
+    Counts motifs in a directed graph
+    From: https://gist.github.com/tpoisot/8582648
+    :param gr: A ``DiGraph`` object
+    :param mo: A ``dict`` of motifs to count
+    :returns: A ``dict`` with the number of each motifs, with the same keys as ``mo``
+    This function is actually rather simple. It will extract all 3-grams from
+    the original graph, and look for isomorphisms in the motifs contained
+    in a dictionary. The returned object is a ``dict`` with the number of
+    times each motif was found.::
+        >>> print mcounter(gr, mo)
+        {'S1': 4, 'S3': 0, 'S2': 1, 'S5': 0, 'S4': 3}
+    """
+    import itertools
+    #This function will take each possible subgraphs of gr of size 3, then
+    #compare them to the mo dict using .subgraph() and is_isomorphic
+    
+    #This line simply creates a dictionary with 0 for all values, and the
+    #motif names as keys
+
+    mcount = dict(zip(mo.keys(), list(map(int, np.zeros(len(mo))))))
+    nodes = gr.nodes()
+
+    #We use iterools.product to have all combinations of three nodes in the
+    #original graph. Then we filter combinations with non-unique nodes, because
+    #the motifs do not account for self-consumption.
+
+    triplets = list(itertools.product(*[nodes, nodes, nodes]))
+    triplets = [trip for trip in triplets if len(list(set(trip))) == 3]
+    triplets = map(list, map(np.sort, triplets))
+    u_triplets = []
+    [u_triplets.append(trip) for trip in triplets if not u_triplets.count(trip)]
+
+    #The for each each of the triplets, we (i) take its subgraph, and compare
+    #it to all fo the possible motifs
+
+    for trip in u_triplets:
+        sub_gr = gr.subgraph(trip)
+        mot_match = list(map(lambda mot_id: nx.is_isomorphic(sub_gr, mo[mot_id]), mo.keys()))
+        match_keys = [list(mo.keys())[i] for i in range(len(mo)) if mot_match[i]]
+        if len(match_keys) == 1:
+            mcount[match_keys[0]] += 1
+
+    return mcount
 
 def match_i_Huskies_passing_table(filename, match_i):
     '''
