2832156fa846d818a4302cee3d38cb23cf0f5cce
Jiaxuan Li <jiaxuan_li@pku.edu.cn>
2020-02-15 17:42:42 +0800

    calculate motifs with weights

--- utils.py
+++ utils.py
@@ -5,12 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def mcounter(gr, mo):
+def mcounter(gr, mo, weight_dict):
     """
     Counts motifs in a directed graph
     From: https://gist.github.com/tpoisot/8582648
+    
     :param gr: A ``DiGraph`` object
     :param mo: A ``dict`` of motifs to count
+    :param weight_dict: ``dict`` of weight of each edge in graph ``gr``
+    
     :returns: A ``dict`` with the number of each motifs, with the same keys as ``mo``
     This function is actually rather simple. It will extract all 3-grams from
     the original graph, and look for isomorphisms in the motifs contained
@@ -27,15 +30,16 @@ def mcounter(gr, mo):
     #motif names as keys
 
     mcount = dict(zip(mo.keys(), list(map(int, np.zeros(len(mo))))))
+    mweight = dict(zip(mo.keys(), list(map(int, np.zeros(len(mo))))))
     nodes = gr.nodes()
 
     #We use iterools.product to have all combinations of three nodes in the
     #original graph. Then we filter combinations with non-unique nodes, because
     #the motifs do not account for self-consumption.
 
-    triplets = list(itertools.product(*[nodes, nodes, nodes]))
-    triplets = [trip for trip in triplets if len(list(set(trip))) == 3]
-    triplets = map(list, map(np.sort, triplets))
+    triplets = list(itertools.product(*[nodes, nodes, nodes])) # all permutaions of 3 nodes
+    triplets = [trip for trip in triplets if len(list(set(trip))) == 3] # remove permutations with duplicate nodes
+    triplets = map(list, map(np.sort, triplets)) 
     u_triplets = []
     [u_triplets.append(trip) for trip in triplets if not u_triplets.count(trip)]
 
@@ -44,12 +48,15 @@ def mcounter(gr, mo):
 
     for trip in u_triplets:
         sub_gr = gr.subgraph(trip)
+        weight_sum = np.sum([weight_dict[edge] for edge in sub_gr.edges])
         mot_match = list(map(lambda mot_id: nx.is_isomorphic(sub_gr, mo[mot_id]), mo.keys()))
         match_keys = [list(mo.keys())[i] for i in range(len(mo)) if mot_match[i]]
         if len(match_keys) == 1:
             mcount[match_keys[0]] += 1
+            mweight[match_keys[0]] += weight_sum
 
-    return mcount
+    return mcount, mweight
+
 
 def match_i_Huskies_passing_table(filename, match_i):
     '''
